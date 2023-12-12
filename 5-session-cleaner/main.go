c050a91c48136fde76a0010fc10eec068c5458ac
Guilherme Silva <guilherme.1995lemes@gmail.com>
2023-12-12 19:02:55 -0300

    6-session-cleaner

--- 5-session-cleaner/main.go
+++ 5-session-cleaner/main.go
@@ -18,19 +18,29 @@
 package main
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"log"
+	"sync"
+	"time"
 )
+
+const sessionTTL time.Duration = 5 * time.Second
 
 // SessionManager keeps track of all sessions from creation, updating
 // to destroying.
 type SessionManager struct {
 	sessions map[string]Session
+	sync     sync.Mutex
 }
 
 // Session stores the session's data
 type Session struct {
-	Data map[string]interface{}
+	Data           map[string]interface{}
+	ExpirationTime time.Time
+	ctx            context.Context
+	cancelFunc     context.CancelFunc
 }
 
 // NewSessionManager creates a new sessionManager
@@ -44,16 +54,51 @@ func NewSessionManager() *SessionManager {
 
 // CreateSession creates a new session and returns the sessionID
 func (m *SessionManager) CreateSession() (string, error) {
+	m.sync.Lock()
+	defer m.sync.Unlock()
+
 	sessionID, err := MakeSessionID()
 	if err != nil {
 		return "", err
 	}
 
-	m.sessions[sessionID] = Session{
-		Data: make(map[string]interface{}),
+	ctx, cancel := context.WithCancel(context.Background())
+	session := Session{
+		Data:           make(map[string]interface{}),
+		ExpirationTime: time.Now().Add(sessionTTL),
+		ctx:            ctx,
+		cancelFunc:     cancel,
 	}
 
+	m.sessions[sessionID] = session
+	go m.CleanSession(sessionID, session)
+
 	return sessionID, nil
+}
+
+func (m *SessionManager) removeSession(sessionID string) {
+	m.sync.Lock()
+	defer m.sync.Unlock()
+
+	delete(m.sessions, sessionID)
+}
+
+func (m *SessionManager) CleanSession(sessionID string, session Session) {
+	ctx := session.ctx
+
+	select {
+	case <-time.After(time.Until(session.ExpirationTime)):
+		select {
+		case <-ctx.Done():
+			fmt.Println("canceling by update")
+		default:
+			fmt.Println("removing")
+			m.removeSession(sessionID)
+		}
+	case <-ctx.Done():
+		fmt.Println("canceling by update")
+	}
+
 }
 
 // ErrSessionNotFound returned when sessionID not listed in
@@ -63,6 +108,9 @@ var ErrSessionNotFound = errors.New("SessionID does not exists")
 // GetSessionData returns data related to session if sessionID is
 // found, errors otherwise
 func (m *SessionManager) GetSessionData(sessionID string) (map[string]interface{}, error) {
+	m.sync.Lock()
+	defer m.sync.Unlock()
+
 	session, ok := m.sessions[sessionID]
 	if !ok {
 		return nil, ErrSessionNotFound
@@ -72,15 +120,25 @@ func (m *SessionManager) GetSessionData(sessionID string) (map[string]interface{
 
 // UpdateSessionData overwrites the old session data with the new one
 func (m *SessionManager) UpdateSessionData(sessionID string, data map[string]interface{}) error {
-	_, ok := m.sessions[sessionID]
+	m.sync.Lock()
+	defer m.sync.Unlock()
+
+	session, ok := m.sessions[sessionID]
 	if !ok {
 		return ErrSessionNotFound
 	}
 
 	// Hint: you should renew expiry of the session here
-	m.sessions[sessionID] = Session{
-		Data: data,
-	}
+
+	session.cancelFunc()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	session.ExpirationTime = time.Now().Add(sessionTTL)
+	session.ctx = ctx
+	session.cancelFunc = cancel
+
+	go m.CleanSession(sessionID, session)
 
 	return nil
 }
